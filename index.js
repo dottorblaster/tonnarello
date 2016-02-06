f35d3a822b42c781f85bb6113667c97b41f4362f
Alessio Biancalana <dottorblaster@gmail.com>
2016-02-07 00:37:38 +0100

    prototyped endpoints

--- index.js
+++ index.js
@@ -2,17 +2,60 @@
 
 const Hapi = require('hapi'),
   config = require('./config.js'),
-  server = new Hapi.Server();
+  Path = require('path'),
+  Inert = require('inert'),
+  server = new Hapi.Server({
+		connections: {
+			routes: {
+				files: {
+					relativeTo: Path.join(__dirname, '.')
+				}
+			}
+		}
+	});
 
 server.connection({
   host: config.host || 'localhost',
   port: config.port || '3000'
 });
+server.register(Inert, function () {});
+
+server.route({
+	method: 'GET',
+	path: '/{param*}',
+	handler: {
+		directory: {
+			path: 'public',
+			defaultExtension: 'html',
+			redirectToSlash: true,
+			listing: false,
+			index: true
+		}
+	}
+});
 
 server.route({
   method: 'POST',
-  path: '/update'
+  path: '/api/new',
   handler: function(req, reply) {
     reply('Hello!');
   }
 });
+
+server.route({
+  method: 'POST',
+  path: '/api/clone',
+  handler: function(req, reply) {
+    reply('Hello!');
+  }
+});
+
+server.start(function (err) {
+	if (err) {
+		console.error('An error occurred: ' + err);
+	} else {
+		console.log('Tonnarello running at:', server.info.uri);
+	}
+});
+
+exports.server = server;
